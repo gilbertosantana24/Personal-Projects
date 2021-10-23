188ea34d04d858726cf6f849a2777590b4924773
gilbertosantana24 <gilbertobuja@gmail.com>
2021-10-22 19:24:41 -0700

    added navbar

--- 07.e-commerce/src/App.js
+++ 07.e-commerce/src/App.js
@@ -1,15 +1,42 @@
 import React, { useState } from 'react';
 import Home from './Views/Home';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './Views/Login';
+import Signup from './Views/Signup';
+
 
 function App() {
 
+  
   return (
+    <Router>
+    <Navbar/>
     <div className="App">
       <header className="App-header">
-      <Home />
+      <Switch>
+        
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+
+          <Route path="/login">
+            <Login />
+          </Route>
+          
+          <Route path="/signup">
+            <Signup />
+          </Route>
+
+          
+        </Switch>
+      
         
       </header>
     </div>
+    </Router>
+   
   );
 }
 
