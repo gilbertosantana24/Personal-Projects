8d7c5edde051c90fd977b0a63c4fc677c7228a25
gilbertosantana24 <gilbertobuja@gmail.com>
2021-11-03 22:02:22 -0700

    item page working

--- 07.e-commerce/src/components/ItemPage.js
+++ 07.e-commerce/src/components/ItemPage.js
@@ -8,29 +8,28 @@ const ItemPage = (props) => {
 
 
     return (
-        
+
         <div className="parent">
 
-        <div className="item_img">
-        <img src={props.item.image} alt="item"/>
+            <div className="item_img">
+                <img src={props.item.image} alt="item" />
+            </div>
+
+            <div className="item_info">
+                <h1>{props.item.product_name}</h1>
+                <p>{props.item.brand}</p>
+                <p>{props.item.description}</p>
+                <p>${props.item.price}</p>
+                <button className="item_pg_btn"
+                    onClick={() => add(props.item.product_name, props.item.price)}>
+                    Add to Cart
+                </button>
+            </div>
+
         </div>
 
-        <div className="item_info">
-        <h1>{props.item.product_name}</h1>
-        <p>{props.item.brand}</p>
-        <p>{props.item.description}</p>
-        <p>${props.item.price}</p>
-        <button className="item_pg_btn" 
-        onClick={() => add(props.item.product_name, props.item.price)}>
-        Add to Cart
-        </button>
 
-        </div>
 
-        </div>
-
-        
-    
 
     )
 }
